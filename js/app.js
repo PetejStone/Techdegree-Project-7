8fae62e8e668f21abe97f4a137711f9fed7ff4a4
PetejStone <petejstoney@gmail.com>
2017-12-12 11:59:24 -0800

    Initial commit
    Added html, css, and JavaScript

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const startButton = document.querySelector('.btn__reset');
+
+//Phrases
+
+const phrases = {
+  One: 'Peter Piper picked a pack of pickled relish',
+  Two: 'I shot the sherif, but I did not shoot the dog',
+  Three: 'Mele Kalikimaka a Hauoli Makahiki Hou',
+  Four: 'Mary had a little llama',
+  Five: 'I hate Mondays',
+  Six: 'There once was a man from Nantucket',
+  Seven: 'To be or not to be, that is the inquiry',
+  Nine: 'Rudolph the blue nose reindeer',
+  Ten: 'Jack and Jill climbed up the ladder',
+};
+
+startButton.addEventListener('click', () => {
+  const overlay = document.querySelector('#overlay');
+  overlay.style.display = 'none';
+})
