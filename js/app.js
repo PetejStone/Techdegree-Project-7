ce84c7ba05838f487b0aa1a16fabf70ab4d7b26e
PetejStone <petejstoney@gmail.com>
2017-12-14 14:55:18 -0800

    Changed object phrases to array
    changed to array

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 //Phrases defined at top so I can use them in variables below
-const phrases = {
-  One: 'Peter Piper picked a pack of pickled relish',
-  Two: 'I shot the sherif but I did not shoot the dog',
-  Three: 'Mele Kalikimaka a Hauoli Makahiki Hou',
-  Four: 'Mary had a little llama',
-  Five: 'I hate Mondays',
-  Six: 'There once was a man from Nantucket',
-  Seven: 'To be or not to be that is the inquiry',
-  Eight: 'Rudolph the blue nose reindeer',
-  Nine: 'Jack and Jill climbed up the ladder',
-};
+const phrases = [
+  'Peter Piper picked a pack of pickled relish',
+  'I shot the sherif but I did not shoot the dog',
+  'Mele Kalikimaka a Hauoli Makahiki Hou',
+  'Mary had a little llama',
+  'I hate Mondays',
+  'There once was a man from Nantucket',
+  'To be or not to be that is the inquiry',
+  'Rudolph the blue nose reindeer',
+  'Jack and Jill climbed up the ladder',
+];
 
 //Random number to be generated between 1 and the number of objects in the arrary
 // Missed variable set to 'zero' to start the game
@@ -59,37 +59,47 @@ const phrases = {
 //Variable with the class name 'letter' added to the name 'correct'
 let correct = document.querySelectorAll('.letter');
 
+
+
 // For loop that loops through the number of classes titled 'correct' and
 // gets the value of their text content. Also creates a new variable named
 // 'show' which will be used to add the class name 'show' to all 'correct' classes
 ///containing that letter
 for (let i = 0; i < correct.length; i += 1) {
-   let guess = correct[i].textContent;
+   let correctAnswer = correct[i].textContent;
    let show = correct[i];
 
 // For loop that loops through the game buttons length and listens for a 'click'
 // on each one.
 //Also set the value to Upper Case on both so that the user did not have to
 //worry about case sensitive answers
-///guess value is logged to console if correct and
-  for (let i = 0; i < gameButtons.length; i += 1) {
-   gameButtons[i].addEventListener('click', () => {
-         if (event.target.textContent.toUpperCase() === guess.toUpperCase()) {
-          console.log(guess);
-          show.className = 'show'; //loops through all the correct items that = the event.target  and adds the class name 'show'
+///guess value is logged to console if correct
 
-         }
+  for (let i = 0; i < gameButtons.length; i += 1) {
+   gameButtons[i].addEventListener('click', function checkLetter(event) {
+         if (event.target.textContent.toUpperCase() === correctAnswer.toUpperCase()) {
+           let letterFound = event.target.textContent;
+           show.className = 'show'; //loops through all the correct items that = the event.target  and adds the class name 'show'
+           return letterFound;
+        } else {
+          return null;
+          checkLetter(event);
+        }
+
        })
      }
 
+
 //Same as above, except it listens for keyboard presses and sets those values
 // to uppercase without worrying about case sensitivity, then adds the class name 'show'
-     window.addEventListener('keypress', () => {
-           if (event.key.toUpperCase() === guess.toUpperCase()) {
-            console.log(guess);
+     window.addEventListener('keypress',() => {
+           if (event.key.toUpperCase() === letterFound.toUpperCase()) {
+            return letterFound;
             show.className = 'show';
 
-           }
+          } else {
+            return null;
+          }
          })
 }
 
